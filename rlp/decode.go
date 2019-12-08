27215fdacf23f793d82c2287c27c70f70dfa39b1
one so <oneso@wannoMacBook-Pro.local>
2019-12-08 10:23:06 +0900

    L400, makeStructDecoder, 返り値確認

--- rlp/decode.go
+++ rlp/decode.go
@@ -397,7 +397,7 @@ func makeStructDecoder(typ reflect.Type) (decoder, error) {
 		for _, f := range fields {
 			err := f.info.decoder(s, val.Field(f.index))
 			if err == EOL {
-				return &decodeError{msg: "too few elements", typ: typ}
+				return &decodeError{msg: "too few elements Hoge2", typ: typ}
 			} else if err != nil {
 				return addErrorContext(err, "."+typ.Field(f.index).Name)
 			}
